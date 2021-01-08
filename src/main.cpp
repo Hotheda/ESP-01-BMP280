2e4f9e92431315b142d3da1e76b6edf45563e2fc
Heda <hotheda@gmail.com>
2021-01-08 16:37:46 +0100

    sensor active

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <Adafruit_BMP280.h>
+
+//For reading battery voltage
+ADC_MODE(ADC_VCC);
+float voltage;
+
+#define POWER_PIN D3
+
+Adafruit_BMP280 BMP_Sensor;
 
 void setup() {
-  // put your setup code here, to run once:
+  voltage = ((float) ((int)(ESP.getVcc() / 10.24 )) / 100 );
+
+  Serial.begin(9600);
+  Serial.print("\nSketch started voltage = "+(String)voltage+"v\n");
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, HIGH);
+  delay(500);
+
+  /*
+  if (!BMP_Sensor.begin(0x76)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }*/
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!BMP_Sensor.begin(0x76)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+  Serial.print("Temprature: ");
+  Serial.print(BMP_Sensor.readTemperature());
+  Serial.print(" *C\n");
+  Serial.print("Pressure: ");
+  Serial.print(BMP_Sensor.readPressure());
+  Serial.print(" *C\n");
+
+  digitalWrite(POWER_PIN, LOW);  
+  delay(5000);
+  digitalWrite(POWER_PIN, HIGH);
+  delay(500);
 }
